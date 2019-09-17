06bdf87d0630fa70b3ad24d8b53391bb5b60e198
thesdev <thesdev@outlook.com>
2019-09-17 22:39:45 +0430

    major changes

--- services/command-services/user-command-service/src/main.rs
+++ services/command-services/user-command-service/src/main.rs
@@ -1,39 +1,18 @@
-//use actix_web::{App, HttpServer};
-use rdkafka::{
-	config::{ClientConfig, FromClientConfig},
-	producer::future_producer::{FutureProducer, FutureRecord},
-};
-use futures::*;
+use actix_web::{web, App, HttpServer};
 
-//mod user;
+mod user;
 
 fn main() {
 	// "postgres://YourUserName:YourPassword@YourHost:5432/YourDatabase"
 	// let con_str = "postgres://postgres:s1031374@localhost:5432/optic_mart";
-//	HttpServer::new(|| {
-//		App::new()
-//			.service(user::register)
-//	})
-//		.bind("127.0.0.1:8088")
-//		.unwrap()
-//		.run()
-//		.unwrap();
+	println!("Listening on 127.0.0.1:8090");
 	
-	
-	let fut_producer: FutureProducer = ClientConfig::new()
-		.set("bootstrap.servers", "localhost:9092")
-		.set("produce.offset.report", "true")
-		.set("message.timeout.ms", "5000")
-		.create()
-		.expect("Producer creation error");
-	
-	let fut_record = FutureRecord::to("test")
-		.payload("lol")
-		.key("2");
-	fut_producer.send(fut_record, 1000)
-		.map(move |delivery_status| {   // This will be executed onw the result is received
-                    	println!("Delivery status for message received ");
-                })
-		.wait();
-	
+	HttpServer::new(|| {
+		App::new()
+			.route("/", web::post().to_async(user::register))
+	})
+		.bind("127.0.0.1:8090")
+		.unwrap()
+		.run()
+		.unwrap();
 }
--- services/command-services/user-command-service/src/user.rs
+++ services/command-services/user-command-service/src/user.rs
@@ -1,5 +1,68 @@
-use actix_web::{post, HttpResponse, Responder};
+use std::convert::From;
+use actix_web::{web, HttpResponse, Responder};
+use uuid::Uuid;
+use bcrypt::{DEFAULT_COST, hash};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use reqwest;
+use futures::Future;
 
-#[post("/")]
-pub fn register() -> impl Responder{
+#[derive(Deserialize)]
+pub struct UserModel {
+	username: String,
+	password: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct User {
+	id: String,
+	username: String,
+	password: String,
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Event {
+	event_id: String,
+	event_type: String,
+	data: String,
+}
+
+impl From<UserModel> for User {
+	fn from(user_model: UserModel) -> Self {
+		Self {
+			id: format!("{}", Uuid::new_v4().to_hyphenated()),
+			username: user_model.username,
+			password: hash(user_model.password, DEFAULT_COST).unwrap(),
+		}
+	}
+}
+
+pub fn register(user_model: web::Json<UserModel>) -> impl Future<Item = impl Responder, Error = actix_web::Error> {
+	let user = User::from(user_model.into_inner());
+	
+	let event = Event {
+		event_id: format!("{}", Uuid::new_v4().to_hyphenated()),
+		event_type: "register".to_owned(),
+		data: serde_json::to_string(&user).unwrap(),
+	};
+	
+	println!("{}", event.event_id);
+	
+	web::block(move ||{
+		reqwest::Client::new()
+			.post("http://127.0.0.1:2113/streams/user")
+			.header("Content-Type", "application/json")
+			.header("ES-CurrentVersion", "-2")
+			.header("ES-EventId", event.event_id)
+			.header("ES-EventType", event.event_type)
+			.json(&user)
+			.send()
+	})
+		.from_err()
+		.and_then(|res| {
+			println!(">>> testing res: {:?}", &res);
+			HttpResponse::Created().body("user registered successfully")
+		})
 }
